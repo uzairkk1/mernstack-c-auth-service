1f5ed49acd93c27c50179b2fe68faa5e65dd97d7
Muhammad Uzair <62392149+uzairkk1@users.noreply.github.com>
2025-02-09 23:04:57 +0500

    setup db

---
+++ src/config/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "../entity/User"
+import { Config } from './index'
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: Config.DB_HOST,
+    port: Number(Config.PORT || 5432),
+    username: Config.DB_USERNAME,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+    synchronize: Config.NODE_ENV !== 'prod',
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class User {
 
     @PrimaryGeneratedColumn()
     id: number
-
-    @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
-
 }
