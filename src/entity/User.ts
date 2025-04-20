04a2279182cda3efb67089992dcdd4cd5afc98e9
uzairkk1 <uzairkhan737@gmail.com>
2025-04-21 02:26:18 +0500

    added tenants FK to users

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
+import {Tenant} from './Tenant'
 
 @Entity({name: 'users'})
 export class User {
@@ -19,4 +20,7 @@ export class User {
 
     @Column()
     role: string
+
+    @ManyToOne(() => Tenant)
+    tenant: Tenant
 }
