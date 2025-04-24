68350755ce18976a08cef2799b85849fbde9e00c
uzairkk1 <uzairkhan737@gmail.com>
2025-04-25 01:31:46 +0500

    Added Tenant CRUD

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -9,19 +9,32 @@ import logger from '../config/logger';
 import authenticate from '../middlewares/authenticate';
 import { canAccess } from '../middlewares/canAccess';
 import { ROLES } from '../constants';
+import tennatValidator from '../validators/tennat-validator';
 
 
 const tenantRepository = AppDataSource.getRepository(Tenant)
 const tenantService = new TenantService(tenantRepository)
 const tenantController = new TenantController(tenantService, logger);
 
-router.post(
-    '/',
+router.route('/').get(authenticate, canAccess([ROLES.ADMIN]), (req: Request, res: Response, next: NextFunction) => {
+    return tenantController.getAll(req, res, next)
+}).post(
     authenticate,
     canAccess([ROLES.ADMIN]),
+    tennatValidator,
     (req: Request, res: Response, next: NextFunction) => {
         return tenantController.create(req, res, next)
     }
 )
+
+router.route("/:id").get(authenticate, canAccess([ROLES.ADMIN]), (req, res, next) => {
+    return tenantController.getOne(req, res, next)
+}).patch(authenticate, canAccess([ROLES.ADMIN]), tennatValidator, (req: Request, res: Response, next: NextFunction) => {
+    return tenantController.update(req, res, next)
+}).delete(authenticate, canAccess([ROLES.ADMIN]), (req, res, next) => {
+    return tenantController.delete(req, res, next)
+})
+
+
 
 export default router
