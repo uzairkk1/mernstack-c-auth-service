023e82d4d8f53eb3e7c518e096276e5efa23053f
uzairkk1 <uzairkhan737@gmail.com>
2025-05-08 02:13:58 +0500

    Added cors and commented build jobs

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,15 @@ import authRouter from './routes/auth'
 import tenantRouter from './routes/tenant'
 import userRouter from './routes/user'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
-
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+        credentials: true,
+    }),
+)
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser())
